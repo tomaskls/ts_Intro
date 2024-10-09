3241d8414e7a7a9f6da3965427255938a3dd642a
tomaskls <tomaskls@hotmail.com>
2024-10-09 17:33:43 +0300

    1 day

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,24 @@
 const n1 = 5;
-const n2 = 7;
+const n2 = 17;
 function sum(a, b) {
-    return a + b;
+    let k = 0;
+    if (typeof a === 'string') {
+        k = parseFloat(a);
+    }
+    let l = 0;
+    if (typeof b === 'string') {
+        l = parseFloat(b);
+    }
+    return k + l;
 }
 console.log(sum(n1, n2));
+console.log(sum(55, "95"));
+function marksAverage(list) {
+    let sum = 0;
+    for (const n of list) {
+        sum += n;
+    }
+    return sum / list.length;
+}
+console.log(marksAverage([1, 2, 3]));
 export {};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 const n1 = 5;
-const n2 = 7;
+const n2 = 17;
 
-function sum(a:number, b:number) {
-    return a + b
+function sum(a:number|string, b:number|string) {
+    let k = 0;
+    if (typeof a === 'string') {
+        k = parseFloat(a);
+    }
+    let l = 0;
+    if (typeof b === 'string') {
+        l = parseFloat(b);
+    }
+    return k + l
 }
 
 console.log(sum(n1,n2));
+console.log(sum(55,"95"));
+
+
+function marksAverage(list:Array<number>) {
+    let sum = 0;
+    for (const n of list){
+      sum += n  
+    }
+    return sum / list.length
+}
+console.log(marksAverage([1, 2, 3]));
+
