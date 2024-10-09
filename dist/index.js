3e856b3a6ca04074771338ba178c0af845608580
tomaskls <tomaskls@hotmail.com>
2024-10-09 10:51:37 +0300

    init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+const n1 = 5;
+const n2 = 7;
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(n1, n2));
+export {};
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const n1 = 5;
+const n2 = 7;
+
+function sum(a:number, b:number) {
+    return a + b
+}
+
+console.log(sum(n1,n2));
