73109cb96d7587f0478095cfee4879e97e898075
tomaskls <tomaskls@hotmail.com>
2024-10-10 10:33:54 +0300

    objects

--- dist/object.js
+++ dist/object.js
@@ -4,6 +4,16 @@ const students = [
         age: 19,
         marks: [10, 2, 8, 6, 4],
     },
+    {
+        name: 'Jonas',
+        age: 19,
+        marks: [10, 2, 8, 6, 4],
+    },
+    {
+        name: 'Petras',
+        age: 29,
+        marks: [10, 2, 8, 6, 4],
+    },
 ];
 export function studentsAverage(student) {
     return 5;
--- src/object.ts
+++ src/object.ts
@@ -1,26 +1,33 @@
-const students: {
+
+type Student = {
     name: string;
     age: number;
     marks: number[];
-}[] = [
-    {
-    name:'Jonas',
-    age:19,
-    marks:[10, 2, 8, 6, 4],
+};
+
+const students: Array<Student> = [
+
+{
+    name: 'Jonas',
+        age: 19,
+            marks: [10, 2, 8, 6, 4],
     },
-    {
-    name:'Jonas',
-    age:19,
-    marks:[10, 2, 8, 6, 4],
+{
+    name: 'Jonas',
+        age: 19,
+            marks: [10, 2, 8, 6, 4],
     },
-    {
-    name:'Petras',
-    age:29,
-    marks:[10, 2, 8, 6, 4],
+{
+    name: 'Petras',
+        age: 29,
+            marks: [10, 2, 8, 6, 4],
     },
 ];
 
 export function studentsAverage(student: { name: string; age: number; marks: number[] }) {
-    return 5
-    
+    let sum = 0;
+}
+for (const n of students.marks){
+    sum += n;
+
 }
